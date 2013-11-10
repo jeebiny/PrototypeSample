8efd46559b640a59a39fa168cd079bd39daae1e1
Seongkwan Yoon <admin@lingostar.co.kr>
2013-11-10 12:26:50 +0900

    LongContentViewController
    Adding LongContentScrollViewController and Storyboard example

---
+++ PrototypeSample/ViewControllers/LSLongContentViewController.h
@@ -0,0 +1,13 @@
+//
+//  LSLongContentViewController.h
+//  PrototypeSample
+//
+//  Created by Lingostar on 2013. 11. 10..
+//  Copyright (c) 2013년 CodersHigh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LSLongContentViewController : UIViewController
+
+@end
---
+++ PrototypeSample/ViewControllers/LSLongContentViewController.m
@@ -0,0 +1,38 @@
+//
+//  LSLongContentViewController.m
+//  PrototypeSample
+//
+//  Created by Lingostar on 2013. 11. 10..
+//  Copyright (c) 2013년 CodersHigh. All rights reserved.
+//
+
+#import "LSLongContentViewController.h"
+
+@interface LSLongContentViewController ()
+
+@end
+
+@implementation LSLongContentViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
